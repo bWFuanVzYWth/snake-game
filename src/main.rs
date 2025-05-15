100cfd7f18643bce34b00ccb0a6b9fe2db392df2
linlin <52383012+bWFuanVzYWth@users.noreply.github.com>
2025-05-16 04:19:34 +0800

    补充大量注释，提高代码可读性

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,6 @@ impl Position {
     }
 }
 
-const fn wrapping_offset(base: usize, offset: usize) -> usize {
-    (base + offset) % MAP_SIZE
-}
-
 #[derive(Debug)]
 struct SnakeGame {
     direction: Position,                 // 记住前进方向
@@ -49,6 +45,7 @@ struct SnakeGame {
 }
 
 impl Default for SnakeGame {
+    /// 返回贪吃蛇的默认上下文。其实就是初始化游戏。
     fn default() -> Self {
         let mut tmp = Self {
             direction: DIRECTION_NONE,
@@ -75,63 +72,77 @@ impl Default for SnakeGame {
 }
 
 impl SnakeGame {
+    /// 用于维护环形队列索引。
+    ///
+    /// 当MAP_SIZE是2的整数次幂时，除法可用位运算取代，甚至直接依赖数据类型的溢出截断
+    const fn wrapping_offset(base: usize, offset: usize) -> usize {
+        (base + offset) % MAP_SIZE
+    }
+
+    /// 在空位中随机选择一个位置
     fn random_food_hashed_position(&self) -> usize {
         let mut rng = rand::rng();
-        let empty_indices_base = wrapping_offset(self.tail_index, self.length);
+        let empty_indices_base = Self::wrapping_offset(self.tail_index, self.length);
         let empty_indices_length = MAP_SIZE - self.length;
-        let empty_indices_random = wrapping_offset(
+        let empty_indices_random = Self::wrapping_offset(
             empty_indices_base,
             rand::Rng::random_range(&mut rng, 0..empty_indices_length),
         );
         self.hashed_positions[empty_indices_random]
     }
 
+    /// 生成食物
     fn generate_food(&mut self) {
         let food_hash = self.random_food_hashed_position();
         self.map[food_hash] = CELL_FOOD;
     }
 
+    /// 弹出蛇尾，更新相关数据结构
     const fn pop_snake_tail(&mut self) -> usize {
-        // 只是弹出蛇尾，不用移动positions中的元素
+        // 仅弹出蛇尾，不涉及positions中元素的变动
         let tail_hash = self.hashed_positions[self.tail_index];
 
-        // 修改tail_index
-        self.tail_index = wrapping_offset(self.tail_index, 1);
+        // 移动指向蛇尾的索引
+        self.tail_index = Self::wrapping_offset(self.tail_index, 1);
 
-        // 修改map
+        // 杂项
         self.map[tail_hash] = CELL_EMPTY;
-
-        // 蛇长--
         self.length -= 1;
 
         tail_hash
     }
 
+    /// 压入蛇头，更新相关的数据结构
     const fn push_snake_head(&mut self, head_hash: usize) {
         // 找到新的蛇头对应的元素
         let new_head_hash = head_hash;
         let new_head_index = self.indices[new_head_hash];
+
         // 找到因为会被覆写，所以需要迁移的元素
-        let relocate_from_index = wrapping_offset(self.tail_index, self.length);
+        let relocate_from_index = Self::wrapping_offset(self.tail_index, self.length);
         let relocate_from_hash = self.hashed_positions[relocate_from_index];
+
         // 交换元素
         self.hashed_positions[new_head_index] = self.hashed_positions[relocate_from_index];
         self.hashed_positions[relocate_from_index] = head_hash;
+
         // 维护因交换元素变化的索引
         self.indices[new_head_hash] = relocate_from_index;
         self.indices[relocate_from_hash] = new_head_index;
 
+        // 杂项
         self.map[new_head_hash] = CELL_SNAKE;
         self.length += 1;
     }
 
     /// 游戏更新的主要逻辑
     ///
+    /// 以下事件的先后顺序不可变更，否则可能产生严重的逻辑错误
     /// 1. 删除蛇尾
-    /// 2. 判断蛇头碰撞
-    /// 3. 插入蛇头
-    /// 4. 生成食物
+    /// 2. 插入蛇头
+    /// 3. 生成食物
     fn update(&mut self, direction: Position) -> u8 {
+        // 处理方向输入
         if direction != DIRECTION_NONE {
             self.direction = direction;
         }
@@ -140,8 +151,8 @@ impl SnakeGame {
             return STATE_READY;
         }
 
-        // 计算新的蛇头位置
-        let head_index = wrapping_offset(self.tail_index, self.length - 1);
+        // 根据当前蛇头位置和方向输入，计算新的蛇头位置
+        let head_index = Self::wrapping_offset(self.tail_index, self.length - 1);
         let head_position = Position::from_hash(self.hashed_positions[head_index]);
         let new_head_position = Position {
             x: (head_position.x + self.direction.x),
@@ -155,9 +166,9 @@ impl SnakeGame {
             return STATE_OVER;
         }
 
-        // 预计算索引
         let new_head_hash = new_head_position.as_hash();
 
+        // 碰撞测试
         match self.map[new_head_hash] {
             CELL_EMPTY => {
                 self.pop_snake_tail();
@@ -182,11 +193,11 @@ impl SnakeGame {
         }
     }
 
+    /// 控制台输出
+    ///
+    /// 不参与核心逻辑，不必关心复杂度，此处仅使用最朴素的实现
     fn render(&self) {
-        const BORDER: &str = "-";
-        const BORDER_LENGTH: usize = MAP_SIDE_LENGTH + 2;
-
-        let border_line = BORDER.repeat(BORDER_LENGTH);
+        let border_line = "-".repeat(MAP_SIDE_LENGTH + 2);
         let mut output = String::with_capacity(MAP_SIZE + 4 * MAP_SIDE_LENGTH);
 
         output.push_str("\x1B[2J\x1B[1;1H");
